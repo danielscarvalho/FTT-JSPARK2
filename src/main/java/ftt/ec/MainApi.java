7518f69bed6722443410beadb4dc6aadb3e597f1
Daniel Carvalho <danielscarvalho@gmail.com>
2021-10-21 20:43:57 -0300

    New apis...

--- src/main/java/ftt/ec/MainApi.java
+++ src/main/java/ftt/ec/MainApi.java
@@ -1,6 +1,8 @@
 package ftt.ec;
 
 import static spark.Spark.*;
+import ftt.ec.controller.*;
+import spark.Spark;
 
 import java.util.Date;
 
@@ -14,14 +16,25 @@ public class MainApi {
 		// https://www.eclipse.org/jetty/download.php
 		// https://sparkjava.com/tutorials/maven-setup
 		// https://books.goalkicker.com/JavaBook/
+		
+		// https://www.baeldung.com/spark-framework-rest-api
+		// https://www.javatpoint.com/spark-java
 	
-		//Microserviço com Java Spark
+		//Microserviço com Java Spark - http://localhost:4567/hello
+		
+		Spark.staticFiles.location("public");
+
 		
 		get("/hello", (req, res) -> "Hello FTT - GET - " + new Date());
 		delete("/hello", (req, res) -> "Hello FTT - DELETE - " + new Date());
 		post("/hello", (req, res) -> "Hello FTT - POST - " + new Date());
 		put("/hello", (req, res) -> "Hello FTT - PUT - " + new Date());
-	
+		
+		//TODO - Daniel Carvalho - API Senha segura...
+	    get("/safepassword", (req,res) -> new SafePassword().getPassword() );
+	    
+	    get("/soma/:a/:b", (req,res) -> new Calc().add(req.params(":a"),req.params(":b")));
+		
 	}
 
 }
